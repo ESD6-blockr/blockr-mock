da99c5ba611c83b66c5635693d5913f421e2b4f8
Roel van den Boom <roelvdboom@gmail.com>
2019-04-09 15:25:10 +0200

    remove auth

--- mock/settings.py
+++ mock/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"]
+    "DEFAULT_PERMISSION_CLASSES": []
 }
 
 WSGI_APPLICATION = "mock.wsgi.application"
